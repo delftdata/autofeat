8fdbb038df754f4bf956cca8b505cf6b975b3048
aionescu <andradenis.ionescu@gmail.com>
2023-05-15 11:51:03 +0200

    Created CLI for ablation and hyper-param tuning

--- src/feature_discovery/cli.py
+++ src/feature_discovery/cli.py
@@ -1,29 +1,30 @@
 from typing import Optional, List
-from typing_extensions import Annotated
 
 import pandas as pd
 import typer
+from typing_extensions import Annotated
 
+from feature_discovery.config import RESULTS_FOLDER
 from feature_discovery.run import (
     filter_datasets,
     get_arda_results,
     get_base_results,
     get_tfd_results,
-    get_classification_results,
+    get_classification_results, get_results_ablation_classification, get_results_tune_value_ratio_classification,
 )
-from feature_discovery.config import RESULTS_FOLDER
 
 app = typer.Typer()
 
 
 @app.command()
 def run_arda(
-    dataset_labels: Annotated[
-        Optional[List[str]], typer.Option(help="Whether to run only on a list of datasets. Filters by dataset labels")
-    ] = None,
-    results_file: Annotated[
-        str, typer.Option(help="CSV file where the results will be written")
-    ] = "results_arda.csv",
+        dataset_labels: Annotated[
+            Optional[List[str]], typer.Option(
+                help="Whether to run only on a list of datasets. Filters by dataset labels")
+        ] = None,
+        results_file: Annotated[
+            str, typer.Option(help="CSV file where the results will be written")
+        ] = "results_arda.csv",
 ):
     """Runs the ARDA experiments."""
     all_results = []
@@ -36,12 +37,13 @@ def run_arda(
 
 @app.command()
 def run_base(
-    dataset_labels: Annotated[
-        Optional[List[str]], typer.Option(help="Whether to run only on a list of datasets. Filters by dataset labels")
-    ] = None,
-    results_file: Annotated[
-        str, typer.Option(help="CSV file where the results will be written")
-    ] = "results_base.csv",
+        dataset_labels: Annotated[
+            Optional[List[str]], typer.Option(
+                help="Whether to run only on a list of datasets. Filters by dataset labels")
+        ] = None,
+        results_file: Annotated[
+            str, typer.Option(help="CSV file where the results will be written")
+        ] = "results_base.csv",
 ):
     """Runs the base experiments."""
     all_results = []
@@ -54,12 +56,14 @@ def run_base(
 
 @app.command()
 def run_tfd(
-    dataset_labels: Annotated[
-        Optional[List[str]], typer.Option(help="Whether to run only on a list of datasets. Filters by dataset labels")
-    ] = None,
-    results_file: Annotated[str, typer.Option(help="CSV file where the results will be written")] = "results_tfd.csv",
-    value_ratio: Annotated[float, typer.Option(help="Value ratio to be used in the TFD experiments")] = 0.55,
-    auto_gluon: Annotated[bool, typer.Option(help="Whether to use AutoGluon")] = True,
+        dataset_labels: Annotated[
+            Optional[List[str]], typer.Option(
+                help="Whether to run only on a list of datasets. Filters by dataset labels")
+        ] = None,
+        results_file: Annotated[
+            str, typer.Option(help="CSV file where the results will be written")] = "results_tfd.csv",
+        value_ratio: Annotated[float, typer.Option(help="Value ratio to be used in the TFD experiments")] = 0.55,
+        auto_gluon: Annotated[bool, typer.Option(help="Whether to use AutoGluon")] = True,
 ):
     """Runs the TFD experiments."""
     all_results = []
@@ -72,17 +76,48 @@ def run_tfd(
 
 @app.command()
 def run_all(
-    dataset_labels: Annotated[
-        Optional[List[str]],
-        typer.Option(help="Whether to run only on a list of datasets. Filters by dataset labels"),
-    ] = None,
-    results_file: Annotated[
-        str, typer.Option(help="CSV file where the results will be written")
-    ] = "all_results_autogluon.csv",
-    value_ratio: Annotated[float, typer.Option(help="Value ratio to be used in the TFD experiments")] = 0.55,
+        dataset_labels: Annotated[
+            Optional[List[str]],
+            typer.Option(help="Whether to run only on a list of datasets. Filters by dataset labels"),
+        ] = None,
+        results_file: Annotated[
+            str, typer.Option(help="CSV file where the results will be written")
+        ] = "all_results_autogluon.csv",
+        value_ratio: Annotated[float, typer.Option(help="Value ratio to be used in the TFD experiments")] = 0.55,
 ):
     """Runs all experiments (ARDA + base + TFD)."""
     get_classification_results(value_ratio, dataset_labels, results_file)
+
+
+@app.command()
+def run_ablation(
+        value_ratio: Annotated[float, typer.Option(help="Value ratio to be used in the TFD experiments")] = 0.55,
+        dataset_labels: Annotated[
+            Optional[List[str]],
+            typer.Option(help="Whether to run only on a list of datasets. Filters by dataset labels"),
+        ] = None,
+        results_file: Annotated[
+            str, typer.Option(help="CSV file where the results will be written")
+        ] = "ablation_study_autogluon.csv",
+):
+    """ Run all the ablation study experiments """
+    datasets = filter_datasets(dataset_labels)
+    get_results_ablation_classification(value_ratio, datasets, results_file)
+
+
+@app.command()
+def run_tune_value_ratio(
+        dataset_labels: Annotated[
+            Optional[List[str]],
+            typer.Option(help="Whether to run only on a list of datasets. Filters by dataset labels"),
+        ] = None,
+        results_file: Annotated[
+            str, typer.Option(help="CSV file where the results will be written")
+        ] = "all_results_value_ratio_tuning.csv",
+):
+    """ Run experiment on the sensitivity of value_ratio hyper-parameter"""
+    datasets = filter_datasets(dataset_labels)
+    get_results_tune_value_ratio_classification(datasets, results_file)
 
 
 if __name__ == "__main__":
