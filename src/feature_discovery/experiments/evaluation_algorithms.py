60d95da434858e9ba8a8768569db8c851922e4ed
aionescu <andradenis.ionescu@gmail.com>
2023-10-31 11:20:48 +0100

    Removed unused code

--- src/feature_discovery/experiments/evaluation_algorithms.py
+++ src/feature_discovery/experiments/evaluation_algorithms.py
@@ -70,110 +70,11 @@ def run_auto_gluon(dataframe: pd.DataFrame, target_column: str, problem_type: st
     return end - start, results
 
 
-def run_svm(dataframe: pd.DataFrame, target_column: str, backward_sel=False, forward_sel=False):
-    from sklearn.svm import LinearSVC
-    from sklearn.feature_selection import RFECV, SequentialFeatureSelector
-
-    start = time.time()
-
-    X = dataframe.drop(columns=[target_column])
-    y = dataframe[[target_column]].values.ravel()
-
-    if backward_sel:
-        selector = RFECV(LinearSVC(dual=False), cv=10)
-        new_X = selector.fit_transform(X, y)
-        X = pd.DataFrame(new_X, columns=selector.get_feature_names_out())
-
-    if forward_sel:
-        selector = SequentialFeatureSelector(LinearSVC(dual=False))
-        new_X = selector.fit_transform(X, y)
-        X = pd.DataFrame(new_X, columns=selector.get_feature_names_out())
-
-    scores = cross_validate(LinearSVC(dual=False), X, y, cv=10, scoring="accuracy", return_estimator=True)
-    estimator = scores["estimator"][np.argmax(scores['test_score'])]
-
-    entry = Result(
-        algorithm="SVM",
-        accuracy=scores['test_score'].mean(),
-        feature_importance=dict(zip(list(X.columns), estimator.coef_[0])),
-        join_path_features=list(X.columns),
-    )
-
-    end = time.time()
-
-    return end - start, entry
-
-
-def run_svm_wrapper(X, y, backward_sel=False, forward_sel=False):
-    from sklearn.svm import LinearSVC
-    from sklearn.feature_selection import RFECV, SequentialFeatureSelector
-
-    start = time.time()
-
-    if backward_sel:
-        selector = RFECV(LinearSVC(dual=False), cv=10)
-        new_X = selector.fit_transform(X, y)
-        new_df = pd.DataFrame(new_X, columns=selector.get_feature_names_out())
-
-    if forward_sel:
-        selector = SequentialFeatureSelector(LinearSVC(dual=False), n_features_to_select='auto', tol=1e-4)
-        new_X = selector.fit_transform(X, y)
-        new_df = pd.DataFrame(new_X, columns=selector.get_feature_names_out())
-
-    end = time.time()
-
-    return end - start, new_df
-
-
-def run_naive_bayes(dataframe: pd.DataFrame, target_column: str):
-    from sklearn.naive_bayes import GaussianNB
-
-    start = time.time()
-
-    X = dataframe.drop(columns=[target_column])
-    y = dataframe[[target_column]].values.ravel()
-    scores = cross_validate(GaussianNB(), X, y, cv=10, scoring="accuracy")
-
-    entry = Result(
-        algorithm="NaiveBayes",
-        accuracy=scores['test_score'].mean(),
-        join_path_features=list(X.columns),
-    )
-
-    end = time.time()
-
-    return end - start, entry
-
-
-def run_logistic_regression(dataframe: pd.DataFrame, target_column: str):
-    from sklearn.linear_model import LogisticRegressionCV
-
-    start = time.time()
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        dataframe.drop(columns=[target_column]),
-        dataframe[[target_column]],
-        test_size=0.2,
-        random_state=10,
-    )
-    clf = LogisticRegressionCV(cv=10, random_state=0).fit(X_train, y_train.values.ravel())
-    entry = Result(
-        algorithm="LogisticRegression",
-        accuracy=clf.score(X_test, y_test),
-        join_path_features=list(X_train.columns),
-        feature_importance=dict(zip(list(X_train.columns), clf.coef_[0])),
-    )
-
-    end = time.time()
-    return end - start, entry
-
-
 def evaluate_all_algorithms(dataframe: pd.DataFrame, target_column: str, problem_tye: str = None):
     df = AutoMLPipelineFeatureGenerator(
         enable_text_special_features=False, enable_text_ngram_features=False
     ).fit_transform(X=dataframe)
 
-    # Run LightGBM, XGBoost, Random Forest, Extreme Randomised Trees
     logging.debug(f"Training AutoGluon ... ")
     runtime, results = run_auto_gluon(
         dataframe=df,
@@ -185,26 +86,5 @@ def evaluate_all_algorithms(dataframe: pd.DataFrame, target_column: str, problem
     for res in results:
         res.train_time = runtime
         res.total_time += res.train_time
-
-    # # sklearn: Run SVM
-    # logging.debug(f"Training SVM ... ")
-    # runtime_svm, result_svm = run_svm(dataframe=df,
-    #                                   target_column=target_column)
-    # result_svm.train_time = runtime_svm
-    # results.append(result_svm)
-    #
-    # # sklearn: Run Naive Bayes
-    # logging.debug(f"Training Naive Bayes ... ")
-    # runtime_nb, result_nb = run_naive_bayes(dataframe=df,
-    #                                         target_column=target_column)
-    # result_nb.train_time = runtime_nb
-    # results.append(result_nb)
-    #
-    # # sklearn: Run Logistic Regression
-    # logging.debug(f"Training Logistic Regression ... ")
-    # runtime_lr, result_lr = run_logistic_regression(dataframe=df,
-    #                                                 target_column=target_column)
-    # result_lr.train_time = runtime_lr
-    # results.append(result_lr)
 
     return results, df
