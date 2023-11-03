755c1fb79b8f69c86cbf56707f032c26cc3dce7d
aionescu <andradenis.ionescu@gmail.com>
2023-11-03 16:40:35 +0100

    update cli

--- src/feature_discovery/config.py
+++ src/feature_discovery/config.py
@@ -11,16 +11,16 @@ DATASET_TYPE = "benchmark"
 
 DATA = "data"
 DATA_FOLDER = ROOT_FOLDER / DATA / DATASET_TYPE
-# RESULTS_FOLDER = ROOT_FOLDER / "results" / "revision-test"
-RESULTS_FOLDER = ROOT_FOLDER / "results"
+RESULTS_FOLDER = ROOT_FOLDER / "results" / "revision-test"
+# RESULTS_FOLDER = ROOT_FOLDER / "results"
 AUTO_GLUON_FOLDER = ROOT_FOLDER / "AutogluonModels"
 
 ### CREDENTIALS ###
 # NEO4J_HOST = os.getenv("NEO4J_HOST", "bolt://localhost:11003")
-NEO4J_HOST = os.getenv("NEO4J_HOST", "neo4j://localhost:7687")
-NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
-NEO4J_PASS = os.getenv("NEO4J_PASS", "password")
+NEO4J_HOST = os.getenv("NEO4J_HOST", "neo4j://172.30.106.77:7687")
+NEO4J_USER = os.getenv("NEO4J_USER", "")
+NEO4J_PASS = os.getenv("NEO4J_PASS", "")
 NEO4J_CREDENTIALS = (NEO4J_USER, NEO4J_PASS)
 
-# NEO4J_DATABASE = os.getenv("NEO4J_DATABASE", DATASET_TYPE)
-NEO4J_DATABASE = "lake"
+NEO4J_DATABASE = os.getenv("NEO4J_DATABASE", DATASET_TYPE)
+# NEO4J_DATABASE = "lake"
